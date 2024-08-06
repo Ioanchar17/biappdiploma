61c0c0b752f901e3f371a0998893429176f05caa
Ioanchar17 <ioanchar17@gmail.com>
2024-08-06 17:46:24 +0300

    Iphone Dataset

--- main.py
+++ main.py
@@ -25,97 +25,96 @@ if selected_dataset == "Macbook Reviews":
     review_pos = visualize_df[visualize_df["rating"] == 'positive'].dropna()
     review_neu = visualize_df[visualize_df["rating"] == 'neutral'].dropna()
     review_neg = visualize_df[visualize_df["rating"] == 'negative'].dropna()
-    tab1, tab2, tab3 = st.tabs(["Reviews Analysis", "Text Analysis", "Sentiment Analysis"])
 
     ## Review Analysis Tab
-    with tab1:
-        col1, col2 = st.columns(2)
-        with col1:
-            st.header("Ratings of reviews")
-            visualize_df = visualize_df.groupby(['rating'])['rating'].count().reset_index(name='count')
-            fig = px.pie(visualize_df, values='count', names='rating', color='rating',
-                         color_discrete_map={'positive': 'skyblue',
-                                             'neutral': 'yellow',
-                                             'negative': 'crimson'})
-            st.plotly_chart(fig)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.header("Ratings of reviews")
+        visualize_df = visualize_df.groupby(['rating'])['rating'].count().reset_index(name='count')
+        fig = px.pie(visualize_df, values='count', names='rating', color='rating',
+                     color_discrete_map={'positive': 'skyblue',
+                                         'neutral': 'yellow',
+                                         'negative': 'crimson'})
+        st.plotly_chart(fig)
 
-        with col2:
-            st.header("Review Rating Distribution")
-            official_df = pd.read_csv('Official_datasets/Apple_Macbook_Air_M1_final.csv')
-            fig = px.histogram(official_df['rating'], x="rating")
-            st.plotly_chart(fig)
+    with col2:
+        st.header("Review Rating Distribution")
+        official_df = pd.read_csv('Official_datasets/Apple_Macbook_Air_M1_final.csv')
+        fig = px.histogram(official_df['rating'], x="rating")
+        st.plotly_chart(fig)
 
-    ## Text Analysis Tab
-    with tab2:
-        option = st.selectbox(
-            "Choose N for N-Gram Analysis",
-            ("1", "2", "3"), index=None)
-        col1, col2, col3 = st.columns(3)
-        if option == "1":
-            with col1:
-                st.write("20 Most Frequent words of Positive Reviews")
-                pos_unigram = get_top_n_grams(review_pos['cleaned_review'], 1, 20)
-                plot_n_gram(pos_unigram, ["skyblue"])
-            with col2:
-                st.write("20 Most Frequent words of Neutral Reviews")
-                neu_unigram = get_top_n_grams(review_neu['cleaned_review'], 1, 20)
-                plot_n_gram(neu_unigram, ["yellow"])
-            with col3:
-                st.write("20 Most Frequent words of Negative Reviews")
-                neg_unigram = get_top_n_grams(review_neg['cleaned_review'], 1, 20)
-                plot_n_gram(neu_unigram, ["crimson"])
-        elif option == "2":
-            with col1:
-                st.write("Bigram plot of Positive Reviews")
-                pos_unigram = get_top_n_grams(review_pos['cleaned_review'], 2, 20)
-                plot_n_gram(pos_unigram, ["skyblue"])
-            with col2:
-                st.write("Bigram plot of Neutral Reviews")
-                neu_unigram = get_top_n_grams(review_neu['cleaned_review'], 2, 20)
-                plot_n_gram(neu_unigram, ["yellow"])
-            with col3:
-                st.write("Bigram plot of Negative Reviews")
-                neg_unigram = get_top_n_grams(review_neg['cleaned_review'], 2, 20)
-                plot_n_gram(neu_unigram, ["crimson"])
-        elif option == "3":
-            with col1:
-                st.write("Trigram plot of Positive Reviews")
-                pos_unigram = get_top_n_grams(review_pos['cleaned_review'], 3, 20)
-                plot_n_gram(pos_unigram, ["skyblue"])
-            with col2:
-                st.write("Trigram plot of Neutral Reviews")
-                neu_unigram = get_top_n_grams(review_neu['cleaned_review'], 3, 20)
-                plot_n_gram(neu_unigram, ["yellow"])
-            with col3:
-                st.write("Trigram plot of Negative Reviews")
-                neg_unigram = get_top_n_grams(review_neg['cleaned_review'], 3, 20)
-                plot_n_gram(neu_unigram, ["crimson"])
+
+    ## Sentiment Analysis Tab
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("My Custom Model (BernoulliNB)")
+        visualize_df_sentiment = clean_reviews.groupby(['predicted_sentiment'])[
+            'predicted_sentiment'].count().reset_index(name='count')
+        visualize_df_sentiment_labels = {0: 'neutral', 1: 'negative', 2: 'positive'}
+        visualize_df_sentiment['predicted_sentiment'] = visualize_df_sentiment['predicted_sentiment'].map(visualize_df_sentiment_labels)
+        fig = px.pie(visualize_df_sentiment, values='count', names='predicted_sentiment',
+                     color='predicted_sentiment',
+                     color_discrete_map={'positive': 'skyblue',
+                                         'neutral': 'yellow',
+                                         'negative': 'crimson'})
+        st.plotly_chart(fig)
+    with col2:
+        st.subheader("VADER Model")
+        visualize_df_sentiment = clean_reviews_vader.groupby(['sentiment'])[
+            'sentiment'].count().reset_index(name='count')
+        fig = px.pie(visualize_df_sentiment, values='count', names='sentiment',
+                     color='sentiment',
+                     color_discrete_map={'positive': 'skyblue',
+                                         'neutral': 'yellow',
+                                         'negative': 'crimson'})
+        st.plotly_chart(fig)
 
     ## Text Analysis Tab
-    with tab3:
-        col1, col2 = st.columns(2)
+
+    option = st.selectbox(
+        "Choose N for N-Gram Analysis",
+        ("1", "2", "3"), index=None)
+    col1, col2, col3 = st.columns(3)
+    if option == "1":
         with col1:
-            st.subheader("My Custom Model (BernoulliNB)")
-            visualize_df_sentiment = clean_reviews.groupby(['predicted_sentiment'])[
-                'predicted_sentiment'].count().reset_index(name='count')
-            visualize_df_sentiment_labels = {0: 'neutral', 1: 'negative', 2: 'positive'}
-            visualize_df_sentiment['predicted_sentiment'] = visualize_df_sentiment['predicted_sentiment'].map(visualize_df_sentiment_labels)
-            fig = px.pie(visualize_df_sentiment, values='count', names='predicted_sentiment',
-                         color='predicted_sentiment',
-                         color_discrete_map={'positive': 'skyblue',
-                                             'neutral': 'yellow',
-                                             'negative': 'crimson'})
-            st.plotly_chart(fig)
+            st.subheader("20 Most Frequent words of Positive Reviews")
+            pos_unigram = get_top_n_grams(review_pos['cleaned_review'], 1, 20)
+            plot_n_gram(pos_unigram, ["skyblue"])
         with col2:
-            st.subheader("VADER Model")
-            visualize_df_sentiment = clean_reviews_vader.groupby(['sentiment'])[
-                'sentiment'].count().reset_index(name='count')
-            fig = px.pie(visualize_df_sentiment, values='count', names='sentiment',
-                         color='sentiment',
-                         color_discrete_map={'positive': 'skyblue',
-                                             'neutral': 'yellow',
-                                             'negative': 'crimson'})
-            st.plotly_chart(fig)
+            st.subheader("20 Most Frequent words of Neutral Reviews")
+            neu_unigram = get_top_n_grams(review_neu['cleaned_review'], 1, 20)
+            plot_n_gram(neu_unigram, ["yellow"])
+        with col3:
+            st.subheader("20 Most Frequent words of Negative Reviews")
+            neg_unigram = get_top_n_grams(review_neg['cleaned_review'], 1, 20)
+            plot_n_gram(neu_unigram, ["crimson"])
+    elif option == "2":
+        with col1:
+            st.subheader("Bigram plot of Positive Reviews")
+            pos_unigram = get_top_n_grams(review_pos['cleaned_review'], 2, 20)
+            plot_n_gram(pos_unigram, ["skyblue"])
+        with col2:
+            st.subheader("Bigram plot of Neutral Reviews")
+            neu_unigram = get_top_n_grams(review_neu['cleaned_review'], 2, 20)
+            plot_n_gram(neu_unigram, ["yellow"])
+        with col3:
+            st.subheader("Bigram plot of Negative Reviews")
+            neg_unigram = get_top_n_grams(review_neg['cleaned_review'], 2, 20)
+            plot_n_gram(neu_unigram, ["crimson"])
+    elif option == "3":
+        with col1:
+            st.subheader("Trigram plot of Positive Reviews")
+            pos_unigram = get_top_n_grams(review_pos['cleaned_review'], 3, 20)
+            plot_n_gram(pos_unigram, ["skyblue"])
+        with col2:
+            st.subheader("Trigram plot of Neutral Reviews")
+            neu_unigram = get_top_n_grams(review_neu['cleaned_review'], 3, 20)
+            plot_n_gram(neu_unigram, ["yellow"])
+        with col3:
+            st.subheader("Trigram plot of Negative Reviews")
+            neg_unigram = get_top_n_grams(review_neg['cleaned_review'], 3, 20)
+            plot_n_gram(neu_unigram, ["crimson"])
 
 else:
     st.write("Loading App......")
